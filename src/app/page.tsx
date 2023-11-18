b5400495d1c8c043779ba456782fcc6561830c58
Y.Gagan <gagannnaidu2006@gmail.com>
2023-11-18 16:40:35 +0530

    giest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h1-bold">
-      <h1 className="text-9xl h1-bold  font-sans flex justify-center items-center m-96">Geist.</h1> </div>
+      <h1 className="text-9xl h1-bold  font-sans text-center m-96">Geist.</h1> </div>
   
   );
 }
