ab88ee0348163e4fd4947aacd1d4e2580bf5e9fd
Y.Gagan <gagannnaidu2006@gmail.com>
2023-12-05 20:10:44 +0530

    some errors we try to fix them

--- src/comp/Navcontent.tsx
+++ src/comp/Navcontent.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { sidebarLinks } from '@/app/constant'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 const Navcontent = () => {
+  console.log(sidebarLinks)
   return (
-    <div>Navcontent</div>
+    <div>
+   <ul>
+    {sidebarLinks.map((link)=>{
+      return (
+        <Link
+        key={link.route}
+        href={link.route}>
+           <Image alt={link.label} height={20} width={20} src={link.imgURL} />
+       
+            <Button className='ml-3 mt-4 border-2  border-primary bg-card-foreground  px-32 text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >{
+              link.label
+            }
+            </Button>
+          
+        </Link>
+      )
+    })}
+   </ul>
+    </div>
   )
 }
 
