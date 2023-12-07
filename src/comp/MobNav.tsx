1b89ea3fe6f01eb18c86941078206bae9ad50a3c
Y.Gagan <gagannnaidu2006@gmail.com>
2023-12-07 16:06:06 +0530

    fixed some bugs but need to contribute more

--- src/comp/MobNav.tsx
+++ src/comp/MobNav.tsx
@@ -1,4 +1,6 @@
+
 import React from 'react'
+
 import {
     Sheet,
     SheetContent,
@@ -9,12 +11,14 @@ import {
   import Link from 'next/link'
   import ham from "../assets/icons/hamburger.svg"
   import Image from 'next/image'
+  // import { usePathname } from 'next/navigation'
 // import Navcontent from './Navcontent'
 import { SignedOut } from '@clerk/nextjs'
 import { Button } from '@/components/ui/button'
 import Navcontent from './Navcontent'
 
 const MobNav = () => {
+
   return (
     <div><Sheet >
     <SheetTrigger asChild>
@@ -23,19 +27,22 @@ const MobNav = () => {
         alt="Hamburger Menu"
         width={36}
         height={36}
-        className='invert dark:invert-0 sm:hidden'
+        className='cursor-pointer invert dark:invert-0 sm:hidden'
         />
     </SheetTrigger>
     <SheetContent side={'left'} className='border-none'>
     <div className='flex justify-center border-b-2 border-muted-foreground'>
-        <Link className='-ml-11 flex' href={'/'}>
-        <Image alt="ygn" height={26} width={26} src={ygn} className=' contrast-100'/>
+      <SheetClose asChild>
+      <Link className='-ml-5 flex' href={'/'}>
+        <Image alt="ygn" height={26} width={26} src={ygn} />
           <span className=' font-sans text-3xl  font-extrabold text-primary'>
           Ygn  </span>
           <span className='mt-2 font-sans text-xl font-bold text-popover-foreground'>Discussions</span></Link>
          
+      </SheetClose>
+        
       </div>
-      <SheetClose>
+      <SheetClose asChild>
       <Navcontent/>
     </SheetClose>
      <SignedOut>
@@ -45,7 +52,7 @@ const MobNav = () => {
      
         <Link href={'/sign-in'}>
           
-          <Button className='ml-3 mt-4 border-2  border-primary bg-card-foreground  px-32 text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >Sign In</Button>
+          <Button className='ml-3 mt-4 w-80  border-2 border-primary bg-card-foreground px-32 text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >Sign In</Button>
           
       </Link>
     
@@ -56,7 +63,7 @@ const MobNav = () => {
      
       <Link href={'/sign-up'}>
         
-        <Button className='ml-3 mt-4 border-2  border-primary bg-card-foreground  px-[124px] text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >Sign Up</Button>
+        <Button className='ml-3 mt-4 w-80  border-2 border-primary bg-card-foreground  text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >Sign Up</Button>
         
     </Link>
   
--- src/comp/Navcontent.tsx
+++ src/comp/Navcontent.tsx
@@ -1,26 +1,36 @@
+
 import React from 'react'
 import { sidebarLinks } from '@/app/constant'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+
+import { SheetClose } from '@/components/ui/sheet'
+
 const Navcontent = () => {
-  console.log(sidebarLinks)
   return (
     <div>
    <ul>
-    {sidebarLinks.map((link)=>{
+    {
+
+    sidebarLinks.map((link)=>{
       return (
-        <Link
-        key={link.route}
+        <SheetClose asChild key={link.route} >
+  <Link
         href={link.route}>
-           <Image alt={link.label} height={20} width={20} src={link.imgURL} />
+          
        
-            <Button className='ml-3 mt-4 border-2  border-primary bg-card-foreground  px-32 text-muted hover:bg-primary hover:text-popover-foreground' variant='destructive' >{
+            <Button className="ml-3 mt-4 w-80 border-2 border-primary bg-card-foreground px-32 text-muted  hover:bg-primary hover:text-popover-foreground active:ring-2 active:ring-primary-foreground " variant='destructive' >
+          
+              {
+                
               link.label
             }
+          
             </Button>
           
         </Link>
+        </SheetClose>
+      
       )
     })}
    </ul>
