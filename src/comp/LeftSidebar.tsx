8d876df2f9cf65b0aa9fa3d89b5c8273608a9627
Y.Gagan <gagannnaidu2006@gmail.com>
2023-12-08 13:16:51 +0530

    want to correct the position of the left side bar

---
+++ src/comp/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    // eslint-disable-next-line tailwindcss/no-contradicting-classname
+    <div className='sticky left-0 top-0   hidden h-screen flex-1 flex-col bg-black  sm:block sm:w-[250px] md:w-[270px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px]  '>
+    </div>
+  )
+}
+
+export default LeftSidebar
